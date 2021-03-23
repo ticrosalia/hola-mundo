e8b1a1c8b63e955a70d221497487403b25a84048
ticrosalia <81256654+ticrosalia@users.noreply.github.com>
2021-03-23 19:06:37 +0100

    Añadimos una cuenta atrás al principio.

--- main.ts
+++ main.ts
@@ -4,6 +4,10 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
 })
-basic.forever(function () {
-	
-})
+let CuentaAtras = 9
+for (let index = 0; index < 9; index++) {
+    basic.showString("" + (CuentaAtras))
+    CuentaAtras = CuentaAtras - 1
+    basic.pause(1000)
+}
+basic.showString("")
