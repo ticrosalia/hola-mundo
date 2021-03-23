43f37c56e046fbc9fa5df56f8dd3b8ffded7ff56
ticrosalia <81256654+ticrosalia@users.noreply.github.com>
2021-03-23 19:00:04 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hola Mundo")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.forever(function () {
+	
+})
